00a0eae14fbd5d52257c98b1b7c8c67d152db048
Ephantus M Muchiru <emuchiru@usiu.ac.ke>
2022-06-04 12:37:46 +0300

    First react app

---
+++ src/Greeter.js
@@ -0,0 +1,19 @@
+const title = 'React';
+
+function Greeter(){
+    return(
+        <div>
+            <h1 className="hello">Hello {title}</h1>
+            
+            <p> 
+            <h1 classname="lorem"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</h1>
+            </p>
+        </div>
+
+    );
+}
+
+export default Greeter;
